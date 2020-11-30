9792972d465f53ecd74f1c362a115e1462f9e921
David Fisher <fisherds@rose-hulman.edu>
2020-11-29 22:53:59 -0500

    Initial Commit

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by David Fisher on 11/29/20.
+//  Copyright © 2020 David Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
